31b156b0399b78bd5d5c51fc73395066b8cb6a1f
Wongsatorn Thongrod <43429376+thongrodw@users.noreply.github.com>
2024-01-16 17:03:46 +0700

    Add files via upload

---
+++ src/main/App.java
@@ -0,0 +1,8 @@
+package com.example.ec.explorecali;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
